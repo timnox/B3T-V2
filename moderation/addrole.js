ca0e688b1a4cbc9e5ebb3dffed46179d9e109089
Timnox <ilyesfougani@gmail.com>
2025-04-22 20:51:05 +0200

    Correction bug quick.db table

--- moderation/addrole.js
+++ moderation/addrole.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const config = require("../config");
 const db = require("quick.db");
-const owner = new db.table("Owner");
+const owner = db.table("Propriétaire");
 const cl = new db.table("Color");
 const ml = new db.table("modlog");
 const pgs = new db.table("PermGs");
