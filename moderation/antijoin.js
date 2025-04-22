1e21d3bfcc76b15761f1c68a35e995cfefd69e0f
Timnox <ilyesfougani@gmail.com>
2025-04-22 21:04:59 +0200

    Ton message de commit ici

--- moderation/antijoin.js
+++ moderation/antijoin.js
@@ -1,10 +1,11 @@
-const Discord = require("discord.js")
-const db = require('quick.db')
-const config = require("../config")
-const owner = new db.table("Owner")
-const cl = new db.table("Color")
-const wl = new db.table("Whitelist")
- 
+const Discord = require("discord.js");
+const { QuickDB } = require('quick.db'); // Importation de QuickDB
+const config = require("../config");
+
+const db = new QuickDB();  // Initialisation de QuickDB
+const owner = db.table("Owner");  // Accéder à la table "Owner"
+const cl = db.table("Color");  // Accéder à la table "Color"
+const wl = db.table("Whitelist");  // Accéder à la table "Whitelist"
 
 module.exports = {
     name: 'antijoin',
@@ -12,37 +13,37 @@ module.exports = {
     description: `Permet de d'interdire l'accès à des vocaux.`,
     async execute(client, message, args) {
 
-        if (owner.get(`owners.${message.author.id}`) || (config.bot.buyer.includes(message.author.id)  ) === true) {
+        if (await owner.get(`owners.${message.author.id}`) || config.bot.buyer.includes(message.author.id)) {
 
-            let color = cl.fetch(`color_${message.guild.id}`)
-            if (color == null) color = config.bot.couleur
+            let color = await cl.get(`color_${message.guild.id}`);
+            if (color == null) color = config.bot.couleur;
 
-            if (args[0] == 'add') {
+            if (args[0] === 'add') {
 
                 const newChannel = message.mentions.channels.first() || message.guild.channels.cache.get(args[1] || message.channelId);
-                if (!newChannel) return message.channel.send({ content: "Aucun salon trouvé !" })
-                if (db.get(`${message.guild.id}.antivoc.${newChannel.id}`) === newChannel) return message.channel.send(`🔊・__Le salon <#${db.get(`${message.guild.id}.antivoc.${newChannel}`)}> est désormais interdit__\n*(Bypass par les owners et la Whitelist)*`)
-                else {
-                    db.set(`${message.guild.id}.antivoc.${newChannel.id}`, newChannel.id)
-                    message.channel.send(`🔊・__Le salon ${newChannel} est désormais interdit__\n*(Bypass: owner et whitelist)*`)
+                if (!newChannel) return message.channel.send({ content: "Aucun salon trouvé !" });
 
+                if (await db.get(`${message.guild.id}.antivoc.${newChannel.id}`)) {
+                    return message.channel.send(`🔊・__Le salon <#${newChannel.id}> est désormais interdit__\n*(Bypass par les owners et la Whitelist)*`);
+                } else {
+                    await db.set(`${message.guild.id}.antivoc.${newChannel.id}`, newChannel.id);
+                    message.channel.send(`🔊・__Le salon ${newChannel} est désormais interdit__\n*(Bypass: owner et whitelist)*`);
                 }
-            }
 
-            if (args[0] == 'remove') {
+            } else if (args[0] === 'remove') {
 
                 const newChannel = message.mentions.channels.first() || message.guild.channels.cache.get(args[1] || message.channelId);
-                db.set(`${message.guild.id}.antivoc.${newChannel.id}`, null)
-                message.channel.send({ content: `Le salon ${newChannel} n'est plus interdit.` })
+                if (!newChannel) return message.channel.send({ content: "Aucun salon trouvé !" });
 
-            }
+                await db.delete(`${message.guild.id}.antivoc.${newChannel.id}`);
+                message.channel.send({ content: `Le salon ${newChannel} n'est plus interdit.` });
 
-            if (args[0] == 'clear') {
+            } else if (args[0] === 'clear') {
 
-                db.delete(`${message.guild.id}.antivoc`)
-                message.channel.send({ content: `Tous les salons interdit sont de nouveau autorisé.` })
+                await db.delete(`${message.guild.id}.antivoc`);
+                message.channel.send({ content: `Tous les salons interdits sont de nouveau autorisés.` });
 
             }
         }
     }
-}
+};
